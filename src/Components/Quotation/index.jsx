c6413c4478e614e1aedab9155f9de5092366bb97
Antônio Sousa <antoniocsousa89@gmail.com>
2025-08-07 18:14:42 -0300

    style(Quotation): organize the quotation elements in Quotation component

--- src/Components/Quotation/index.jsx
+++ src/Components/Quotation/index.jsx
@@ -4,8 +4,15 @@ import './style.css'
 export default function Quotation() {
     return (
         <section className='quotation-container'>
-            <SelectBox />
-            <SelectBox />
+            <div className="info-box">
+                <h2>R$ 5,52</h2>
+                <p>1 dólar americano hoje em real brasileiro</p>
+            </div>
+            <div className="quotation-box">
+                <SelectBox />
+                <img src="src\Assets\transfer.svg" alt="sla" />
+                <SelectBox />
+            </div>
         </section>
     ) 
 }
